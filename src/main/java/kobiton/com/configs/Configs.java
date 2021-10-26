5fd16c499b20e54b4b1e6a2e7ba3ff8f825d021c
kinzu22 <truong.longnhat22@gmail.com>
2021-10-26 10:06:17 +0700

    config

--- src/main/java/kobiton/com/configs/Configs.java
+++ src/main/java/kobiton/com/configs/Configs.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Configs {
 
-    static String USERNAME = "";
-    static String ACCESS_KEY = "";
+    static String USERNAME = "hoangpm";
+    static String ACCESS_KEY = "30096c3e-f66e-4465-895c-7beba68d6030";
     public static final URL kobitonServerUrl(){
         try {
             return new URL("https://" + USERNAME + ":" + ACCESS_KEY + "@api.kobiton.com/wd/hub");
@@ -46,8 +46,8 @@ public class Configs {
         capabilities.setCapability("deviceOrientation", "portrait");
         capabilities.setCapability("captureScreenshots", true);
         capabilities.setCapability("app", "https://s3-ap-southeast-1.amazonaws.com/kobiton-devvn/apps-test/demo/com.dozuki.ifixit.apk");
-        capabilities.setCapability("deviceGroup", "KOBITON");
-        capabilities.setCapability("deviceName", "Galaxy S5");
+        capabilities.setCapability("deviceGroup", "*");
+        capabilities.setCapability("deviceName", "*");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("allowMobileShell", true);
         return capabilities;
